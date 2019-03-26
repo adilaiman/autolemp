7a18486e6f7f21942a3fd198c5af8282540cfad7
Adi Laiman <laiman@posteo.de>
2019-03-26 00:25:25 +0000

    Upload script and config

---
+++ nginx-mysql-php.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Run this script chmod +x nginx-mysql-php.sh && ./nginx-mysql-php.sh
+
+# make sure script is being run as root
+[[ $EUID -ne 0 ]] && echo "Please run this script as sudo." && exit 1
+
+# install nginx and allow through firewall
+sudo apt install nginx
+sudo ufw allow 'Nginx HTTP'
+
+# install mysql and set root password
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD123'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD123'
+sudo apt install mysql-server
+sudo mysql_secure_installation
+
+# install php
+sudo apt install php-fpm php-mysql
+
+# config files
+sudo less nginxconfig > /etc/nginx/default
+
+echo "All done :)."
